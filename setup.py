8502f4bd62e601c33341fed7d337ea4a30b1be79
Zhiguo Zhu <zhiguo.zhu@nio.com>
2022-06-24 11:37:41 +0800

    elmfatfs upgrade to R0.14b version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='efatfs',
-    version="0.0.7",
+    version="0.0.8",
     author="Ladislav Laska",
     author_email="krakonos@krakonos.org",
     description="A wrapper around ChaN's FatFS library for FAT filesystem manipulation.",
